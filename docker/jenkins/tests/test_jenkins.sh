a21b994de4bd2c284e8bae3f28bc559be2d2bce7
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-03-18 11:17:42 +1100

    Updated the script to add retry mechanism to wait for Jenkins to be ready.

--- docker/jenkins/tests/test_jenkins.sh
+++ docker/jenkins/tests/test_jenkins.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
 JENKINS_URL="http://localhost:8080"
+MAX_RETRIES=50
+SLEEP_INTERVAL=2
 
-if ! curl -fs "$JENKINS_URL/login" | grep -q "Sign in to Jenkins"; then
-  echo "❌ Jenkins is NOT ready!"
-  exit 1
-fi
+echo "Waiting for Jenkins to be ready..."
 
-echo "✅ Jenkins is running!"
+for i in $(seq 1 $MAX_RETRIES); do
+  if curl -fs "$JENKINS_URL/login" | grep -q "Sign in to Jenkins"; then
+    echo "✅ Jenkins is running!"
+    exit 0
+  fi
+  echo "Retrying in $SLEEP_INTERVAL seconds... ($i/$MAX_RETRIES)"
+  sleep $SLEEP_INTERVAL
+done
+
+echo "❌ Jenkins did not start in time!"
+exit 1
