0a98862ec03f67f497e7def8f7465bc852da630a
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-03-25 09:55:54 +1100

    Renamed a variable.

--- docker/jenkins/scripts/builder_entrypoint.sh
+++ docker/jenkins/scripts/builder_entrypoint.sh
@@ -6,10 +6,10 @@ echo "Logging into local registry..."
 docker login -u "${REGISTRY_USER}" -p "${REGISTRY_PASSWORD}" "${REGISTRY_URL}"
 
 echo "Building agent image..."
-docker build -t "${JENKINS_AGENT_IMAGE_PATH}" -f /build-context/Dockerfile.agent /build-context
+docker build -t "${JENKINS_DOCKER_AGENT_IMAGE_PATH}" -f /build-context/Dockerfile.agent /build-context
 
 echo "Pushing agent image to local registry..."
-docker push "${JENKINS_AGENT_IMAGE_PATH}"
+docker push "${JENKINS_DOCKER_AGENT_IMAGE_PATH}"
 
 docker images
 echo "Done."
