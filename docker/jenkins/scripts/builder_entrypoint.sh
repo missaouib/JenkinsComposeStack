53c09eadbe27f43a8f81fccf732d4981fb1e29bc
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-03-25 08:41:33 +1100

    Extracted agent image path to a variable.

--- docker/jenkins/scripts/builder_entrypoint.sh
+++ docker/jenkins/scripts/builder_entrypoint.sh
@@ -6,10 +6,10 @@ echo "Logging into local registry..."
 docker login -u "${REGISTRY_USER}" -p "${REGISTRY_PASSWORD}" "${REGISTRY_URL}"
 
 echo "Building agent image..."
-docker build -t "${REGISTRY_URL}/jenkins_agent:latest" -f /build-context/Dockerfile.agent /build-context
+docker build -t "${JENKINS_AGENT_IMAGE_PATH}" -f /build-context/Dockerfile.agent /build-context
 
 echo "Pushing agent image to local registry..."
-docker push "${REGISTRY_URL}/jenkins_agent:latest"
+docker push "${JENKINS_AGENT_IMAGE_PATH}"
 
 docker images
 echo "Done."
