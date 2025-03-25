613555289452909191fe1be38ff227cf81ed8fcd
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-03-25 12:13:10 +1100

    Updated the script to build and push the custom image only if there is a change in the corresponding Dockerfile.

--- docker/jenkins/scripts/builder_entrypoint.sh
+++ docker/jenkins/scripts/builder_entrypoint.sh
@@ -5,11 +5,19 @@ set -e  # Exit on any error
 echo "Logging into local registry..."
 echo "${REGISTRY_PASSWORD}" | docker login --username "${REGISTRY_USER}" --password-stdin "${REGISTRY_URL}"
 
-echo "Building agent image..."
-docker build -t "${JENKINS_DOCKER_AGENT_IMAGE_PATH}" -f /build-context/Dockerfile.agent /build-context
+DOCKERFILE_PATH="/build-context/Dockerfile.agent"
+DOCKERFILE_HASH=$(sha256sum "${DOCKERFILE_PATH}" | awk '{print $1}')
+EXISTING_HASH=$(docker image inspect "${JENKINS_DOCKER_AGENT_IMAGE_PATH}" --format '{{ index .Config.Labels "dockerfile-hash" }}' 2>/dev/null || echo 'none')
 
-echo "Pushing agent image to local registry..."
-docker push "${JENKINS_DOCKER_AGENT_IMAGE_PATH}"
+if [ "$DOCKERFILE_HASH" != "$EXISTING_HASH" ]; then
+    echo '🔄 Dockerfile changed! Rebuilding and pushing the custom docker agent image...'
+    docker build --label dockerfile-hash="$DOCKERFILE_HASH" -t "${JENKINS_DOCKER_AGENT_IMAGE_PATH}" -f "${DOCKERFILE_PATH}" /build-context &&
+    docker push "${JENKINS_DOCKER_AGENT_IMAGE_PATH}"
+else
+    echo '✅ Image is up-to-date, skipping build.'
+fi
+
+docker logout  "${REGISTRY_URL}"
 
 docker images
 echo "Done."
